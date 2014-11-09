7068f0cc71a9b62f14d2a3f5e02023673d8d3459
Qingping Hou <dave2008713@gmail.com>
2014-11-09 14:18:29 -0800

    update base image to ubuntu 14.04

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "hashicorp/precise64"
+  config.vm.box = "ubuntu/trusty64"
 
   # The url from where the 'config.vm.box' box will be fetched if it
   # doesn't already exist on the user's system.
@@ -122,4 +122,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # chef-validator, unless you changed the configuration.
   #
   #   chef.validation_client_name = "ORGNAME-validator"
+  config.vm.provider "virtualbox" do |v|
+    v.cpus = 4
+  end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,28 @@
 
 VG_HOME_DIR="/home/vagrant"
 
+# for linux-libc-dev:i386, which is needed by LuaJIT
+dpkg --add-architecture i386
+
+apt-get update
+
+KINDLE_TC="gcc-arm-linux-gnueabi g++-arm-linux-gnueabi"
+KOBO_TC="gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf"
+LIB32_GCC_DEV="lib32gcc-4.8-dev libx32gcc1 libx32gomp1 libx32itm1 "`
+             `"libx32atomic1 libx32asan0 libx32quadmath0 libc6-x32"
+MISC_TOOLS="git subversion zip unzip vim"
+LUAJIT_DEPS="$LIB32_GCC_DEV libc6-dev-amd64:i386"
+GLIB_DEPS="gettext"
+
 echo " ------------------------------------------"
 echo "| installing dependencies..."
+echo "| $LUAJIT_DEPS "
 echo " ------------------------------------------"
-apt-get update
 apt-get install -y \
-	git subversion \
-	zip unzip vim ia32-libs \
-	gcc-arm-linux-gnueabi g++-arm-linux-gnueabi \
-	gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf \
-	g++ make automake cmake ccache patch gettext libtool \
-	autoconf2.64 \
-	build-essential gcc-multilib \
+	$MISC_TOOLS \
+	build-essential dpkg-dev \
+	g++ make automake cmake ccache patch libtool nasm autoconf2.64 \
+	$GLIB_DEPS \
+	$KINDLE_TC $KOBO_TC \
+	$LUAJIT_DEPS \
 	libsdl1.2-dev
