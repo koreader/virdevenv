1a6fdd27a9cfbb6fb109a110b1788dbf4f9e2fa5
Qingping Hou <dave2008713@gmail.com>
2016-04-11 00:39:28 -0700

    add gdb

--- vm/ubuntu/bootstrap.sh
+++ vm/ubuntu/bootstrap.sh
@@ -11,7 +11,7 @@ KINDLE_TC="gcc-arm-linux-gnueabi g++-arm-linux-gnueabi"
 KOBO_TC="gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf"
 LIB32_GCC_DEV="lib32gcc-4.8-dev libx32gcc1 libx32gomp1 libx32itm1 "`
              `"libx32atomic1 libx32asan0 libx32quadmath0 libc6-x32"
-MISC_TOOLS="git subversion zip unzip vim"
+MISC_TOOLS="git subversion zip unzip vim gdb"
 LUAJIT_DEPS="$LIB32_GCC_DEV libc6-dev-amd64:i386"
 GLIB_DEPS="gettext"
 
