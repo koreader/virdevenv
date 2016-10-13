e20d5de86874c1cc86f7b501335e017d1bc4c4fa
Qingping Hou <dave2008713@gmail.com>
2016-10-12 19:51:54 -0700

    support make po

--- docker/ubuntu/kokindle/build_kindle_tc.sh
+++ docker/ubuntu/kokindle/build_kindle_tc.sh
@@ -5,9 +5,9 @@ git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain
 git checkout 47acb959b6169b5d1e546814d1747a8f88484b1b
 
-./gen-tc.sh kindle
-./gen-tc.sh kindle5
-./gen-tc.sh kindlepw2
+./gen-tc.sh kindle || ./gen-tc.sh kindle
+./gen-tc.sh kindle5 || ./gen-tc.sh kindle5
+./gen-tc.sh kindlepw2 || ./gen-tc.sh kindlepw2
 popd
 
 rm -rf koxtoolchain
