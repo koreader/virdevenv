f063733f5522e21e8d9dab8652b297a0ad05ff0b
Qingping Hou <dave2008713@gmail.com>
2016-11-07 10:14:51 -0800

    nightswatcher: build zsync metadata

--- docker/ubuntu/nightswatcher/nightswatcher.py
+++ docker/ubuntu/nightswatcher/nightswatcher.py
@@ -32,15 +32,17 @@ GITLAB_TOKEN = os.environ['GITLAB_WEBHOOK_TOKEN']
 GITLAB_TRIGGER_TOKEN = os.environ['GITLAB_TRIGGER_TOKEN']
 TMP_DATA_DIR = os.environ.get('TMP_DATA_DIR', '/data')
 APK_SIGN_KEY_STORE_PATH = os.environ['APK_SIGN_KEY_STORE_PATH']
-BUILD_DIR = '/data/release_download'
+OTA_DIR = '/data/ota/'
+BUILD_DIR = '/data/release_download/'
 ARTIFACT_URL = ('https://gitlab.com/koreader/nightly-builds'
                 '/builds/%s/artifacts/download')
-nightly_build_dir = '%s/nightly' % BUILD_DIR
+NIGHTLY_BUILD_DIR = BUILD_DIR + 'nightly'
 # Matching:
 # koreader-ubuntu-touch-arm-linux-gnueabihf-v2015.11-640-g17e9a8e.targz
 # koreader-android-arm-linux-androideabi-v2015.11-654-gb7392f7.apk
 artifact_re = re.compile(
-    '.*/koreader-.*-v[0-9]{4}.[0-9]{2}-[0-9]+-g[0-9a-z]{7}\.[a-z]+.*')
+    ('.*/koreader-([a-z\-]+)-arm-.*-'
+     'v[0-9]{4}.[0-9]{2}-[0-9]+-g[0-9a-z]{7}\.([a-z]+).*'))
 version_re = re.compile(
     'koreader-.*-(v[0-9]{4}.[0-9]{2}-[0-9]+-g[0-9a-z]{7})\.[a-z]+')
 
@@ -75,74 +77,108 @@ def sign_apk(apk_path):
     logger.info('Signing %s...', apk_path)
     re = gevent.subprocess.check_output(
         ['jarsigner', '-verbose', '-sigalg', 'SHA1withRSA',
-            '-digestalg', 'SHA1', '-tsa', 'http://timestamp.digicert.com',
-            '-keystore', APK_SIGN_KEY_STORE_PATH,
-            '-keypass', APK_SIGN_KEY_PASS,
-            '-storepass', APK_SIGN_STORE_PASS,
-            apk_path, 'koreader_release'])
+         '-digestalg', 'SHA1', '-tsa', 'http://timestamp.digicert.com',
+         '-keystore', APK_SIGN_KEY_STORE_PATH,
+         '-keypass', APK_SIGN_KEY_PASS,
+         '-storepass', APK_SIGN_STORE_PASS,
+         apk_path, 'koreader_release'])
     logger.info('Output from jarsigner:\n%s', re)
 
 
-def extract_build(zip_name, build):
+def get_artifact_metadata(artifact_zip):
     try:
-        zf = zipfile.ZipFile(zip_name)
+        zf = zipfile.ZipFile(artifact_zip)
     except zipfile.BadZipfile:
-        # caller is responsible for removing the zipfile
-        logger.error('Got invalid zip file: %s', zip_name)
+        logger.error('Got invalid zip file: %s', artifact_zip)
         return
 
-    artifacts = [os.path.basename(f.strip())
-                 for f in zf.namelist()
-                 if artifact_re.match(f)]
+    platform = None
+    artifact = {}
+    for f in zf.namelist():
+        m = artifact_re.match(f)
+        if not m:
+            continue
+        platform, ftype = m.groups()
+        artifact[ftype] = os.path.basename(f.strip())
+
     zf.close()
-    if not artifacts:
+    return platform, artifact
+
+
+download_artifact_ext_map = {
+    'build_android': 'apk',
+    'build_ubuntutouch': 'click',
+}
+
+
+def extract_build(artifact_zip, build):
+    # caller is responsible for removing artifact_zip
+    platform, artifact = get_artifact_metadata(artifact_zip)
+
+    # validate artifact_zip
+    if 'targz' not in artifact:
+        logger.error('Invalid build artifact, missing targz file.')
+        return
+    download_artifact_ext = download_artifact_ext_map.get(build['name'], 'zip')
+    if download_artifact_ext not in artifact:
+        logger.error('Invalid build artifact, missing %s file.',
+                     download_artifact_ext)
         return
 
-    version = version_re.match(artifacts[0]).group(1)
-    version_dir = '%s/%s/' % (nightly_build_dir, version)
-    if all([os.path.exists(version_dir+fname) for fname in artifacts]):
-        logger.info('Skipping because build already extracted.')
+    # check to see if we already have the build
+    version = version_re.match(artifact['targz']).group(1)
+    version_dir = '%s/%s/' % (NIGHTLY_BUILD_DIR, version)
+
+    download_artifact = artifact[download_artifact_ext]
+    download_artifact_path = version_dir + download_artifact
+    if os.path.exists(download_artifact_path):
+        logger.info('Skipping because %s already exists.',
+                    download_artifact_path)
         return
 
+    if not os.path.exists(version_dir):
+        os.mkdir(version_dir)
+
+    # unzip to tmp directory
+    tmp_version_dir = '%s/tmp-%s-%s/' % (TMP_DATA_DIR, platform, version)
+    # -n for no overwrite, -j for junk path
+    unzip_cmd = ['unzip', '-n', '-j', '-d', tmp_version_dir, artifact_zip]
+    run_cmd(unzip_cmd)
+
+    tmp_artifact_path = tmp_version_dir + download_artifact
     if build['name'] == 'build_android':
-        tmp_version_dir = '%s/tmp-%s' % (TMP_DATA_DIR, version)
-        # -n for no overwrite, -j for junk path
-        unzip_cmd = ['unzip', '-n', '-j', '-d', tmp_version_dir, zip_name]
-        run_cmd(unzip_cmd)
-        # android build needs extra signing
-        sign_apk(os.path.join(
-            tmp_version_dir,
-            'koreader-android-arm-linux-androideabi-%s.apk' % version
-        ))
-        if not os.path.exists(version_dir):
-            os.mkdir(version_dir)
-        for artifact in artifacts:
-            shutil.copy2(os.path.join(tmp_version_dir, artifact), version_dir)
-        shutil.rmtree(tmp_version_dir)
-    else:
-        # -n for no overwrite, -j for junk path
-        unzip_cmd = ['unzip', '-n', '-j', '-d', version_dir, zip_name]
-        run_cmd(unzip_cmd)
+        sign_apk(tmp_artifact_path)
+    shutil.move(tmp_artifact_path, download_artifact_path)
+
+    # build zsync metadata for kindle, kobo and pocketbook OTA
+    if build['name'] in ['build_kindle', 'build_legacy_kindle',
+                         'build_kobo', 'build_pocketbook']:
+        zsync_file = OTA_DIR + ('koreader-%s-latest-nightly.zsync' % platform)
+        shutil.move(tmp_version_dir + artifact['targz'], OTA_DIR)
+        run_cmd(['zsyncmake', OTA_DIR + artifact['targz'],
+                 '-u', artifact['targz'], '-o', zsync_file])
+
+    shutil.rmtree(tmp_version_dir)
 
 
 def fetch_build(build):
     logger.info('Fetching artifacts for build %s(%s): %s',
                 build['name'], build['id'], build['artifacts_file'])
-    zip_name = '%s/%s_artifacts.zip' % (TMP_DATA_DIR, build['id'])
+    artifact_zip = '%s/%s_artifacts.zip' % (TMP_DATA_DIR, build['id'])
     # -C - for continue download from dropped off
     retcode = run_cmd(['curl', '--retry', '3', '-C', '-',
-                       ARTIFACT_URL % build['id'], '-o', zip_name])
+                       ARTIFACT_URL % build['id'], '-o', artifact_zip])
     if retcode != 0:
         logger.error('Failed to download build %s(%s)',
                      build['name'], build['id'])
     else:
-        extract_build(zip_name, build)
-    os.remove(zip_name)
+        extract_build(artifact_zip, build)
+    os.remove(artifact_zip)
 
 
 def fetch_build_worker():
-    if not os.path.exists(nightly_build_dir):
-        os.mkdir(nightly_build_dir)
+    if not os.path.exists(NIGHTLY_BUILD_DIR):
+        os.mkdir(NIGHTLY_BUILD_DIR)
     while True:
         logger.info('Fetch build worker waiting for new builds....')
         gevent.spawn(fetch_build, build_fetch_queue.get()).join(timeout=60)
@@ -185,6 +221,7 @@ class PipeLine():
 def init():
     gevent.spawn(fetch_build_worker)
     gevent.spawn(trigger_build)
+    # TODO: purge old nightly targz
 
 
 api = falcon.API()
