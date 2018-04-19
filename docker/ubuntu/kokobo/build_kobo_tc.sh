13c37dcc21d6e8acffe53e0d88e2a6e3562bd169
Frans de Jonge <fransdejonge@gmail.com>
2018-04-19 12:46:11 +0200

    Docker: minor TC update for C++ apps linking to libstdc++ (#14)
    See https://github.com/koreader/koreader/issues/3302 and https://github.com/koreader/koxtoolchain/pull/3

--- docker/ubuntu/kokobo/build_kobo_tc.sh
+++ docker/ubuntu/kokobo/build_kobo_tc.sh
@@ -3,7 +3,7 @@
 echo "Building toolchains for kobo..."
 git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain
-git checkout 2a79651be67b273beb6329bdd249f9e1fdf55e7b
+git checkout 052d82036c6a310a950e8ec93065b4a8be9c8f1e
 
 ./gen-tc.sh kobo
 popd
