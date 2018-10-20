68cd0004434754078ffce801c6386fdde2dbfc21
Martín Fernández <paziusss@gmail.com>
2018-10-20 21:02:38 +0200

    Docker: add kocervantes (#16)

---
+++ docker/ubuntu/kocervantes/build_cervantes_tc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building toolchains for cervantes..."
+git clone https://github.com/koreader/koxtoolchain.git
+pushd koxtoolchain
+git checkout b212bd2f61fcbe0a2bc2d3c4085d7b95a4f79a48
+
+./gen-tc.sh cervantes
+popd
+
+rm -rf koxtoolchain
