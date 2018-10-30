5a151b4103f4f954b8bf2cf2ca2e71826d3cd704
Frans de Jonge <fransdejonge@gmail.com>
2018-10-30 14:01:16 +0100

    [Docker] Update baseimage to Ubuntu 16.04 (#26)
    * [Docker] Update baseimage to Ubuntu 16.04
    
    Fixes #24.
    
    * Split out appimage and make secondary baseimage with Python2 for Android, PB & Transifex

--- docker/ubuntu/kokobo/build_kobo_tc.sh
+++ docker/ubuntu/kokobo/build_kobo_tc.sh
@@ -3,7 +3,7 @@
 echo "Building toolchains for kobo..."
 git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain && {
-    git checkout 052d82036c6a310a950e8ec93065b4a8be9c8f1e
+    git checkout b212bd2f61fcbe0a2bc2d3c4085d7b95a4f79a48
 
     ./gen-tc.sh kobo
 } && popd || exit
