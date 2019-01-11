e2ee1568856e5e6689ac71a6fb6f0c0c8f7ac68a
Frans de Jonge <fransdejonge@gmail.com>
2019-01-11 21:04:06 +0100

    [Docker] Update to Android NDK r15c (#30)
    Necessary prerequisite for
    <https://github.com/koreader/koreader/issues/4471>.

--- docker/ubuntu/koandroid/fetch_android_tc.sh
+++ docker/ubuntu/koandroid/fetch_android_tc.sh
@@ -2,6 +2,6 @@
 
 echo "Downloading NDK..."
 rm Makefile
-wget https://raw.githubusercontent.com/koreader/koreader-base/03d3baf3d8bcbcbd60199ee41ecc5d1df66f24de/toolchain/Makefile
-make android-ndk android-sdk
+wget https://raw.githubusercontent.com/koreader/koreader-base/fc019eff80e3d645335cd501c92ba751ee2f1f76/toolchain/Makefile
+echo -e "y" | make android-ndk android-sdk
 rm Makefile
