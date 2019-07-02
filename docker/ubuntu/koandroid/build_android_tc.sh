3f1dd4630f4ba0098b336cc0d6868af7506cad0d
Frans de Jonge <fransdejonge@gmail.com>
2019-07-02 14:53:53 +0200

    Create fake dir to silence gradle complaint

--- docker/ubuntu/koandroid/build_android_tc.sh
+++ docker/ubuntu/koandroid/build_android_tc.sh
@@ -6,6 +6,9 @@ NDK=android-ndk-r15c
 rm -rf ${NDK}/platforms/android-{9,12,13,15,16,17,18,19,21,22,23,24,26}
 # only keep arm and x86
 rm -rf ${NDK}/toolchains/{aarch64-linux-android-4.9,mips64el-linux-android-4.9,mipsel-linux-android-4.9,x86_64-4.9}
+# create fake dir to keep gradle from complaining
+# see https://github.com/koreader/virdevenv/pull/40
+mkdir -p ${NDK}/toolchains/mips64el-linux-android/prebuilt/linux-x86_64
 
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >>~/.bashrc
 echo 'export NDK=/home/ko/android-ndk-r15c' >>./.bashrc
