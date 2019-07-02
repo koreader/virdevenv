4b6d569713632c6891ef131837de8adb8f02d5ce
Frans de Jonge <fransdejonge@gmail.com>
2019-07-02 18:59:19 +0200

    Docker/Android: bump TC creation (#40)
    Cf. https://github.com/koreader/koreader/pull/5096
    
    * Create fake dir to silence gradle complaint

--- docker/ubuntu/koandroid/build_android_tc.sh
+++ docker/ubuntu/koandroid/build_android_tc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "Pruning NDK to reduce image size..."
-NDK=android-ndk
-# only keep NDK 19
-rm -rf ${NDK}/platforms/android-{12,13,14,15,16,17,18,21,22,23,24,26}
-# only keep arm
-rm -rf ${NDK}/toolchains/{aarch64-linux-android-4.9,mips64el-linux-android-4.9,mipsel-linux-android-4.9,x86-4.9,x86_64-4.9}
+NDK=android-ndk-r15c
+# only keep NDK 14
+rm -rf ${NDK}/platforms/android-{9,12,13,15,16,17,18,19,21,22,23,24,26}
+# only keep arm and x86
+rm -rf ${NDK}/toolchains/{aarch64-linux-android-4.9,mips64el-linux-android-4.9,mipsel-linux-android-4.9,x86_64-4.9}
+# create fake dir to keep gradle from complaining
+# see https://github.com/koreader/virdevenv/pull/40
+mkdir -p ${NDK}/toolchains/mips64el-linux-android/prebuilt/linux-x86_64
 
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >>~/.bashrc
 echo 'export NDK=/home/ko/android-ndk-r15c' >>./.bashrc
--- docker/ubuntu/koandroid/fetch_android_tc.sh
+++ docker/ubuntu/koandroid/fetch_android_tc.sh
@@ -2,6 +2,6 @@
 
 echo "Downloading NDK..."
 rm Makefile
-wget https://raw.githubusercontent.com/koreader/koreader-base/fc019eff80e3d645335cd501c92ba751ee2f1f76/toolchain/Makefile
+wget https://raw.githubusercontent.com/koreader/koreader-base/f07042f179127b73bc0a2d0b751fb1fdeadaac4f/toolchain/Makefile
 echo -e "y" | make android-ndk android-sdk
 rm Makefile
