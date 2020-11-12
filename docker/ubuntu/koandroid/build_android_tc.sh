ffb24b13f91636b08cdfbcdbdffdaa5b9f6ea4a7
Martín Fernández <paziusss@gmail.com>
2020-11-12 16:12:59 +0100

    Add 64 bit ABIs, bump SDK_VER
    Requires https://github.com/koreader/koreader-base/pull/1230

--- docker/ubuntu/koandroid/build_android_tc.sh
+++ docker/ubuntu/koandroid/build_android_tc.sh
@@ -2,10 +2,10 @@
 
 echo "Pruning NDK to reduce image size..."
 NDK=android-ndk-r15c
-# only keep NDK 14
-rm -rf ${NDK}/platforms/android-{9,12,13,15,16,17,18,19,21,22,23,24,26}
-# only keep arm and x86
-rm -rf ${NDK}/toolchains/{aarch64-linux-android-4.9,mips64el-linux-android-4.9,mipsel-linux-android-4.9,x86_64-4.9}
+# only keep platform-14 (32bit ABIs) and platform-21 (64bit ABIs) 
+rm -rf ${NDK}/platforms/android-{9,12,13,15,16,17,18,19,22,23,24,26}
+# remove obsolete android ABIs
+rm -rf ${NDK}/toolchains/{mips64el-linux-android-4.9,mipsel-linux-android-4.9}
 # create fake dir to keep gradle from complaining
 # see https://github.com/koreader/virdevenv/pull/40
 mkdir -p ${NDK}/toolchains/mips64el-linux-android/prebuilt/linux-x86_64
@@ -16,5 +16,5 @@ echo 'export ANDROID_HOME=/home/ko/android-sdk-linux' >>~/.bashrc
 # shellcheck disable=SC2016
 echo 'export PATH=$NDK:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$PATH' >>~/.bashrc
 
-ANDROID_SDK_VER=24.0.3
+ANDROID_SDK_VER=30.0.2
 echo "export PATH=\$ANDROID_HOME/build-tools/${ANDROID_SDK_VER}:\$PATH" >>~/.bashrc
