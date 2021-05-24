a68a352e348653b78e360563db2c11232260d0ec
Frans de Jonge <fransdejonge@gmail.com>
2021-05-24 19:04:57 +0200

    Docker: update Kindle to Ubuntu 18.04

--- docker/ubuntu/kokindle/build_kindle_tc.sh
+++ docker/ubuntu/kokindle/build_kindle_tc.sh
@@ -3,11 +3,11 @@
 echo "Building toolchains for kindle..."
 git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain && {
-    git checkout 576338981f3ba6723801c3056ae2c7ca33915181
+    git checkout e0e19f55d7b5485f6d69394ddd8dcf7c55a28359
 
-    ./gen-tc.sh kindle || ./gen-tc.sh kindle
-    ./gen-tc.sh kindle5 || ./gen-tc.sh kindle5
-    ./gen-tc.sh kindlepw2 || ./gen-tc.sh kindlepw2
+    ./gen-tc.sh kindle
+    ./gen-tc.sh kindle5
+    ./gen-tc.sh kindlepw2
 } && popd || exit
 
 rm -rf koxtoolchain
