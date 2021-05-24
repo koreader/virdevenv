5e26402c77b05ded0ee437ef8f83cf94f7075540
Frans de Jonge <fransdejonge@gmail.com>
2021-05-24 16:04:42 +0200

    Docker: update Bookeen to Ubuntu 18.04 and latest koxtoolchain

--- docker/ubuntu/kobookeen/build_bookeen_tc.sh
+++ docker/ubuntu/kobookeen/build_bookeen_tc.sh
@@ -3,7 +3,7 @@
 echo "Building toolchains for bookeen..."
 git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain && {
-    git checkout 78aceb1bd470ff5b1acc016613ce12ac33f286c6
+    git checkout ca4481e00fb8b6081c578c0241185444c7ca5ff3
 
     ./gen-tc.sh bookeen
 } && popd || exit
