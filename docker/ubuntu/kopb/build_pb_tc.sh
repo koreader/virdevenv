5369b8e2ba0ce9b54ecf1c3951341918e165e96f
Frans de Jonge <fransdejonge@gmail.com>
2021-05-24 18:23:18 +0200

    Docker: update PocketBook to Ubuntu 18.04

--- docker/ubuntu/kopb/build_pb_tc.sh
+++ docker/ubuntu/kopb/build_pb_tc.sh
@@ -3,7 +3,7 @@
 echo "Building toolchain for PocketBook..."
 git clone https://github.com/koreader/koxtoolchain.git
 pushd koxtoolchain && {
-    git checkout 711acc41e2582c63873c4f1a29696cfb6f1a247a
+    git checkout e0e19f55d7b5485f6d69394ddd8dcf7c55a28359
 
     ./gen-tc.sh pocketbook
 } && popd || exit
