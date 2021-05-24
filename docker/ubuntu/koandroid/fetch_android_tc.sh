00b446875d4e9a7f1d01cb7219145d2a25480cb6
Frans de Jonge <fransdejonge@gmail.com>
2021-05-24 23:45:59 +0200

    [fix] Docker baseimage: add libc6-dev:i386 (#63)
    An oversight for LuaJIT cross-compilation on newer systems.
    
    Also swap in GCC 8 instead of 7 since apparently that's an easy option.

--- docker/ubuntu/koandroid/fetch_android_tc.sh
+++ docker/ubuntu/koandroid/fetch_android_tc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -e
 
 echo "Downloading NDK..."
-rm Makefile
 wget https://raw.githubusercontent.com/koreader/koreader-base/d21a0b680832c911675daf035130a0e190e5d386/toolchain/Makefile
 echo -e "y" | make android-ndk android-sdk
 rm Makefile
