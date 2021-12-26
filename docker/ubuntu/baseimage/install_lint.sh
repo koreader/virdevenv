aa98f38b7ba03a85fae913364e1e8eaa4cf27c48
yparitcher <y@paritcher.com>
2021-12-26 00:38:21 -0500

    Aloow install scripts to be used by koreader CI

--- docker/ubuntu/baseimage/install_lint.sh
+++ docker/ubuntu/baseimage/install_lint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HOME=/home/ko
+HOME="$1"
 
 cd $HOME || exit
 
@@ -9,15 +9,15 @@ export PATH=${HOME}/local/bin:$PATH
 [ ! -d "${HOME}/local/bin" ] && mkdir -p "${HOME}/local/bin"
 
 #install our own updated shellcheck
-SHELLCHECK_VERSION="v0.7.2"
+SHELLCHECK_VERSION="v0.8.0"
 SHELLCHECK_URL="https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION?}/shellcheck-${SHELLCHECK_VERSION?}.linux.x86_64.tar.xz"
-if ! command -v shellcheck; then
+if [ "$(shellcheck --version)" != "v0.8.0" ]; then
     curl -sSL "${SHELLCHECK_URL}" | tar --exclude 'SHA256SUMS' --strip-components=1 -C "${HOME}/local/bin" -xJf -
     chmod +x "${HOME}/local/bin/shellcheck"
-    shellcheck --version
 else
     echo -e "${ANSI_GREEN}Using system shellcheck."
 fi
+shellcheck --version
 
 # install shfmt
 SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.0/shfmt_v3.4.0_linux_amd64"
