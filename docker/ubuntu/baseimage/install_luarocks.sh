83e6fe2d25c01b4f1cfde50966d01e3f9e47b748
yparitcher <y@paritcher.com>
2021-12-30 23:02:02 -0500

    Use distro luarocks| Install packages globally
    Use latest busted
    
    dont install luacov-coveralls & luasec: we don't use them at all
    
    There is no need to explicitly install dependencies

--- docker/ubuntu/baseimage/install_luarocks.sh
+++ docker/ubuntu/baseimage/install_luarocks.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
-HOME=/home/ko
+apt-get install --no-install-recommends luarocks
 
-cd $HOME || exit
+luarocks install luacov
+luarocks install ldoc
 
-git clone https://github.com/torch/luajit-rocks.git
-pushd luajit-rocks && {
-    git checkout 2c7496b905f6f972673effda4884766433b7583b
-    cmake . -DWITH_LUAJIT21=ON -DCMAKE_INSTALL_PREFIX=${HOME}/local
-    make install
-} && popd || exit
-rm -rf luajit-rocks
+luarocks install lanes # for parallel luacheck
+luarocks install luacheck
 
-mkdir ${HOME}/.luarocks
-cp ${HOME}/local/etc/luarocks/config.lua ${HOME}/.luarocks/config.lua
-
-export PATH=$HOME/local/bin:$PATH
-
-echo "wrap_bin_scripts = false" >>${HOME}/.luarocks/config.lua
-luarocks --local install luafilesystem
-luarocks --local install ansicolors
-luarocks --local install busted 2.0.0-1
-luarocks --local install luacov
-# luasec doesn't automatically detect 64-bit libs
-luarocks --local install luasec OPENSSL_LIBDIR=/usr/lib/x86_64-linux-gnu
-luarocks --local install luacov-coveralls --server=http://rocks.moonscript.org/dev
-luarocks --local install luacheck
-luarocks --local install lanes # for parallel luacheck
-luarocks --local install ldoc
+luarocks build https://raw.githubusercontent.com/Olivine-Labs/busted/2e4799e06b865c352baa7f7721e32aedaafd19d6/busted-scm-2.rockspec
