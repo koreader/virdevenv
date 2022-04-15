ae1cca50e20aca444bc13f80c48292858f6ebe94
Frans de Jonge <fransdejonge@gmail.com>
2022-04-15 16:15:24 +0200

    Revert "Allow install scripts to be used by koreader CI" (#75)
    * Revert "Use distro luarocks| Install packages globally"
    
    This reverts commit a64fd88f13f20bbf2f24b6045358a1e66973d4bc.
    
    * Revert "Aloow install scripts to be used by koreader CI"
    
    This reverts commit aa98f38b7ba03a85fae913364e1e8eaa4cf27c48.

--- docker/ubuntu/baseimage/install_luarocks.sh
+++ docker/ubuntu/baseimage/install_luarocks.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-apt-get install --no-install-recommends luarocks
+HOME=/home/ko
 
-luarocks install luacov
-luarocks install ldoc
+cd $HOME || exit
 
-luarocks install lanes # for parallel luacheck
-luarocks install luacheck
+git clone https://github.com/torch/luajit-rocks.git
+pushd luajit-rocks && {
+    git checkout 2c7496b905f6f972673effda4884766433b7583b
+    cmake . -DWITH_LUAJIT21=ON -DCMAKE_INSTALL_PREFIX=${HOME}/local
+    make install
+} && popd || exit
+rm -rf luajit-rocks
 
-luarocks build https://raw.githubusercontent.com/Olivine-Labs/busted/2e4799e06b865c352baa7f7721e32aedaafd19d6/busted-scm-2.rockspec
+mkdir ${HOME}/.luarocks
+cp ${HOME}/local/etc/luarocks/config.lua ${HOME}/.luarocks/config.lua
+
+export PATH=$HOME/local/bin:$PATH
+
+echo "wrap_bin_scripts = false" >>${HOME}/.luarocks/config.lua
+luarocks --local install luafilesystem
+luarocks --local install ansicolors
+luarocks --local install busted 2.0.0-1
+luarocks --local install luacov
+# luasec doesn't automatically detect 64-bit libs
+luarocks --local install luasec OPENSSL_LIBDIR=/usr/lib/x86_64-linux-gnu
+luarocks --local install luacov-coveralls --server=http://rocks.moonscript.org/dev
+luarocks --local install luacheck
+luarocks --local install lanes # for parallel luacheck
+luarocks --local install ldoc
