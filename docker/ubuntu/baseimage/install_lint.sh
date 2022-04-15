956325f88cec601ebf4e99b7dad6c0c89fb8e659
Frans de Jonge <fransdejonge@gmail.com>
2022-04-15 16:25:19 +0200

    shellcheck 0.8.0 and shfmt 3.4.3 (#76)

--- docker/ubuntu/baseimage/install_lint.sh
+++ docker/ubuntu/baseimage/install_lint.sh
@@ -9,7 +9,7 @@ export PATH=${HOME}/local/bin:$PATH
 [ ! -d "${HOME}/local/bin" ] && mkdir -p "${HOME}/local/bin"
 
 #install our own updated shellcheck
-SHELLCHECK_VERSION="v0.7.2"
+SHELLCHECK_VERSION="v0.8.0"
 SHELLCHECK_URL="https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION?}/shellcheck-${SHELLCHECK_VERSION?}.linux.x86_64.tar.xz"
 if ! command -v shellcheck; then
     curl -sSL "${SHELLCHECK_URL}" | tar --exclude 'SHA256SUMS' --strip-components=1 -C "${HOME}/local/bin" -xJf -
@@ -20,8 +20,8 @@ else
 fi
 
 # install shfmt
-SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.0/shfmt_v3.4.0_linux_amd64"
-if [ "$(shfmt --version)" != "v3.4.0" ]; then
+SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.3/shfmt_v3.4.3_linux_amd64"
+if [ "$(shfmt --version)" != "v3.4.3" ]; then
     curl -sSL "${SHFMT_URL}" -o "${HOME}/local/bin/shfmt"
     chmod +x "${HOME}/local/bin/shfmt"
 else
