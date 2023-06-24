8268e86f6d8eae9439d81e87e9b3a0c81a4495b3
Benoit Pierre <benoit.pierre@gmail.com>
2023-06-24 18:36:42 +0200

    android: update to use NDK 23c

--- docker/ubuntu/koandroid/build_android_tc.sh
+++ docker/ubuntu/koandroid/build_android_tc.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-echo "Pruning NDK to reduce image size..."
-NDK=android-ndk-r15c
-# only keep platform-14 (32bit ABIs) and platform-21 (64bit ABIs)
-rm -rf ${NDK}/platforms/android-{9,12,13,15,16,17,18,19,22,23,24,26}
-# remove obsolete android ABIs
-rm -rf ${NDK}/toolchains/{mips64el-linux-android-4.9,mipsel-linux-android-4.9}
-# create fake dir to keep gradle from complaining
-# see https://github.com/koreader/virdevenv/pull/40
-mkdir -p ${NDK}/toolchains/mips64el-linux-android/prebuilt/linux-x86_64
+NDK=android-ndk-r23c
+
+# Hardlink duplicates (reduce size by about ~700MB).
+hardlink ${NDK}
 
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >>~/.bashrc
-echo 'export NDK=/home/ko/android-ndk-r15c' >>./.bashrc
+echo "export ANDROID_NDK_HOME=/home/ko/${NDK}" >>./.bashrc
 echo 'export ANDROID_HOME=/home/ko/android-sdk-linux' >>~/.bashrc
 # shellcheck disable=SC2016
 echo 'export PATH=$NDK:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$PATH' >>~/.bashrc
