38bdf15745821195bce6353558ed4e5910c7fc43
Frans de Jonge <frans@clevercast.com>
2025-01-03 21:39:39 +0100

    nightswatcher: make sure ota targz doesn't accidentally include kindle-legacy

--- docker/ubuntu/nightswatcher/nightswatcher.py
+++ docker/ubuntu/nightswatcher/nightswatcher.py
@@ -275,7 +275,7 @@ def extract_build(artifact_zip, build):
                 nightly_targz = f.readline().decode('utf-8', errors='ignore').split()[1]
 
         for f in os.listdir(OTA_DIR):
-            if f.startswith(f'koreader-{platform}-') and f.endswith('.targz') and f != stable_targz and f != nightly_targz and f != nightly_targz_prev:
+            if f.startswith(f'koreader-{platform}-v') and f.endswith('.targz') and f != stable_targz and f != nightly_targz and f != nightly_targz_prev:
                 logger.info(f'Purging old targz: {f}')
                 os.remove(OTA_DIR + f)
 
