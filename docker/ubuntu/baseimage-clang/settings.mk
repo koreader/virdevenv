67e79d095025ffdd9e6a38d74a25a921f444ce0e
Noa Himesaka <himesaka@noa.codes>
2025-04-21 20:12:52 +0900

    fix: properly bump version

--- docker/ubuntu/baseimage-clang/settings.mk
+++ docker/ubuntu/baseimage-clang/settings.mk
@@ -1,3 +1,3 @@
 IMAGE = kobase-clang
-VERSION = 0.3.1-22.04
+VERSION = 0.3.7-22.04
 BASE = $(BASEIMAGE)
--- docker/ubuntu/baseimage-python/settings.mk
+++ docker/ubuntu/baseimage-python/settings.mk
@@ -1,3 +1,3 @@
 IMAGE = kobase-python
-VERSION = 0.3.1
+VERSION = 0.3.0
 BASE = koreader/kobase:0.3.1-22.04
