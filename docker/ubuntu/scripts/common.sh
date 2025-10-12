ee4e8857ea4aa4242da72583ac4df3d06f7b42a1
Benoit Pierre <benoit.pierre@gmail.com>
2025-10-12 22:17:38 +0200

    fix scripts platform detection

--- docker/ubuntu/scripts/common.sh
+++ docker/ubuntu/scripts/common.sh
@@ -10,7 +10,7 @@ die() {
 if [ -z "${TARGETPLATFORM}" ]; then
     platform="$(uname -s -m | tr 'A-Z ' 'a-z/')"
     case "${platform}" in
-        linux/arm64) TARGETPLATFORM=linux/arm64 ;;
+        linux/aarch64) TARGETPLATFORM=linux/arm64 ;;
         linux/armv7l) TARGETPLATFORM=linux/arm/v7 ;;
         linux/x86_64) TARGETPLATFORM=linux/amd64 ;;
         *) die "unsupported platform: ${platform}" ;;
